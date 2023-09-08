9e0a914b277b95d13d80fb231134c87bce9f70e7
shubham karad <shubham.karad12@gmail.com>
2023-09-08 17:36:29 +0530

    USER report fuctionality

--- src/app/component/side-navigation/users-report/users-report.component.ts
+++ src/app/component/side-navigation/users-report/users-report.component.ts
@@ -14,6 +14,15 @@ export class UsersReportComponent {
   activeSubIndexTab: any=0;
   tableTitleValue: string='Vendors';
   selectedFromTable: any;
+  process: any;
+  subprocess: any;
+  stagewise: any;
+  Batchwise: any;
+  userwise: any;
+  vendorwise: any;
+  currentLevel: any;
+  currentStage=0;
+  level: any=1;
   constructor(public _router: Router, private _route: ActivatedRoute, public authService: AuthService, public http: ApiServiceService) { }
   activeIndexTab: any = 0;
   selectedDropdownValue:any;
@@ -54,55 +63,484 @@ export class UsersReportComponent {
   }
   ]
 
-  tableContent=[{
-    "UserName":'User1',
-    "AHT":'0000000',
-    "FCR":'000%',
-    "Quality":'000%',
-    'Service_level':'000%',
-    'Performance':'High'
+  tableContent = [{
+    "UserName": 'User1',
+    "AHT": '0000000',
+    "FCR": '000%',
+    "Quality": '000%',
+    'Service_level': '000%',
+    'Performance': 'High',
+    'process': [{
+      "UserName": 'User1',
+      "AHT": '0000000',
+      "FCR": '000%',
+      "Quality": '000%',
+      'Service_level': '000%',
+      'Performance': 'High',
+      'subprocess': [{
+        "UserName": 'User1',
+        "AHT": '0000000',
+        "FCR": '000%',
+        "Quality": '000%',
+        'Service_level': '000%',
+        'Performance': 'High',
+        'stagewise': [{
+          "UserName": 'User1',
+          "AHT": '0000000',
+          "FCR": '000%',
+          "Quality": '000%',
+          'Service_level': '000%',
+          'Performance': 'High',
+          'Batchwise': [{
+            "UserName": 'User1',
+            "AHT": '0000000',
+            "FCR": '000%',
+            "Quality": '000%',
+            'Service_level': '000%',
+            'Performance': 'High',
+            'Userwise': [{
+              "UserName": 'User1',
+              "AHT": '0000000',
+              "FCR": '000%',
+              "Quality": '000%',
+              'Service_level': '000%',
+              'Performance': 'High',
+
+            }]
+
+          }]
+
+        }]
+
+
+      }]
+
+
+    },
+    {
+      "UserName": 'User2',
+      "AHT": '0000000',
+      "FCR": '000%',
+      "Quality": '000%',
+      'Service_level': '000%',
+      'Performance': 'Low',
+      'subprocess': [{
+        "UserName": 'User2',
+        "AHT": '0000000',
+        "FCR": '000%',
+        "Quality": '000%',
+        'Service_level': '111%',
+        'Performance': 'High',
+        'stagewise': [{
+          "UserName": 'User2',
+          "AHT": '0000000',
+          "FCR": '000%',
+          "Quality": '000%',
+          'Service_level': '000%',
+          'Performance': 'High',
+          'Batchwise': [{
+            "UserName": 'User2',
+            "AHT": '0000000',
+            "FCR": '000%',
+            "Quality": '000%',
+            'Service_level': '000%',
+            'Performance': 'High',
+            'Userwise': [{
+              "UserName": 'User2',
+              "AHT": '0000000',
+              "FCR": '000%',
+              "Quality": '000%',
+              'Service_level': '000%',
+              'Performance': 'High',
+
+            }]
+
+          }]
+
+        }]
+
+
+      }]
+
+
+    },
+  ]
+
   },
   {
-    "UserName":'User2',
-    "AHT":'0000000',
-    "FCR":'000%',
-    "Quality":'000%',
-    'Service_level':'000%',
-    'Performance':'High'
+    "UserName": 'User2',
+    "AHT": '0000000',
+    "FCR": '000%',
+    "Quality": '000%',
+    'Service_level': '000%',
+    'Performance': 'High',
+    'process': [{
+      "UserName": 'User1',
+      "AHT": '0000000',
+      "FCR": '000%',
+      "Quality": '000%',
+      'Service_level': '000%',
+      'Performance': 'High',
+      'subprocess': [{
+        "UserName": 'User1',
+        "AHT": '0000000',
+        "FCR": '000%',
+        "Quality": '000%',
+        'Service_level': '000%',
+        'Performance': 'High',
+        'stagewise': [{
+          "UserName": 'User1',
+          "AHT": '0000000',
+          "FCR": '000%',
+          "Quality": '000%',
+          'Service_level': '000%',
+          'Performance': 'High',
+          'Batchwise': [{
+            "UserName": 'User1',
+            "AHT": '0000000',
+            "FCR": '000%',
+            "Quality": '000%',
+            'Service_level': '000%',
+            'Performance': 'High',
+            'Userwise': [{
+              "UserName": 'User1',
+              "AHT": '0000000',
+              "FCR": '000%',
+              "Quality": '000%',
+              'Service_level': '000%',
+              'Performance': 'High',
+
+            }]
+
+          }]
+
+        }]
+
+
+      }]
+
+
+    }]
+
+
   },
   {
-    "UserName":'User3',
-    "AHT":'0000000',
-    "FCR":'000%',
-    "Quality":'000%',
-    'Service_level':'000%',
-    'Performance':'Moderate'
+    "UserName": 'User3',
+    "AHT": '0000000',
+    "FCR": '000%',
+    "Quality": '000%',
+    'Service_level': '000%',
+    'Performance': 'Moderate',
+    'process': [{
+      "UserName": 'User1',
+      "AHT": '0000000',
+      "FCR": '121%',
+      "Quality": '90%',
+      'Service_level': '000%',
+      'Performance': 'High',
+      'subprocess': [{
+        "UserName": 'User1',
+        "AHT": '0000000',
+        "FCR": '000%',
+        "Quality": '000%',
+        'Service_level': '000%',
+        'Performance': 'High',
+        'stagewise': [{
+          "UserName": 'User1',
+          "AHT": '0000000',
+          "FCR": '000%',
+          "Quality": '000%',
+          'Service_level': '000%',
+          'Performance': 'High',
+          'Batchwise': [{
+            "UserName": 'User1',
+            "AHT": '0000000',
+            "FCR": '000%',
+            "Quality": '000%',
+            'Service_level': '000%',
+            'Performance': 'High',
+            'Userwise': [{
+              "UserName": 'User1',
+              "AHT": '0000000',
+              "FCR": '000%',
+              "Quality": '000%',
+              'Service_level': '000%',
+              'Performance': 'High',
+
+            }]
+
+          }]
+
+        }]
+
+
+      }]
+
+
+    },
+    {
+      "UserName": 'User1',
+      "AHT": '0000000',
+      "FCR": '121%',
+      "Quality": '90%',
+      'Service_level': '000%',
+      'Performance': 'Moderate',
+      'subprocess': [{
+        "UserName": 'User1',
+        "AHT": '0000000',
+        "FCR": '000%',
+        "Quality": '000%',
+        'Service_level': '000%',
+        'Performance': 'High',
+        'stagewise': [{
+          "UserName": 'User1',
+          "AHT": '0000000',
+          "FCR": '000%',
+          "Quality": '000%',
+          'Service_level': '000%',
+          'Performance': 'High',
+          'Batchwise': [{
+            "UserName": 'User1',
+            "AHT": '0000000',
+            "FCR": '000%',
+            "Quality": '000%',
+            'Service_level': '000%',
+            'Performance': 'High',
+            'Userwise': [{
+              "UserName": 'User1',
+              "AHT": '0000000',
+              "FCR": '000%',
+              "Quality": '000%',
+              'Service_level': '000%',
+              'Performance': 'Moderate',
+
+            }]
+
+          }]
+
+        }]
+
+
+      }]
+
+
+    },
+    {
+      "UserName": 'User1',
+      "AHT": '0000000',
+      "FCR": '113%',
+      "Quality": '110%',
+      'Service_level': '000%',
+      'Performance': 'High',
+      'subprocess': [{
+        "UserName": 'User1',
+        "AHT": '0000000',
+        "FCR": '000%',
+        "Quality": '000%',
+        'Service_level': '000%',
+        'Performance': 'High',
+        'stagewise': [{
+          "UserName": 'User1',
+          "AHT": '0000000',
+          "FCR": '000%',
+          "Quality": '000%',
+          'Service_level': '000%',
+          'Performance': 'High',
+          'Batchwise': [{
+            "UserName": 'User1',
+            "AHT": '0000000',
+            "FCR": '000%',
+            "Quality": '000%',
+            'Service_level': '000%',
+            'Performance': 'High',
+            'Userwise': [{
+              "UserName": 'User1',
+              "AHT": '0000000',
+              "FCR": '000%',
+              "Quality": '000%',
+              'Service_level': '000%',
+              'Performance': 'Moderate',
+
+            }]
+
+          }]
+
+        }]
+
+
+      }]
+
+
+    },
+    
+  ]
   },
   {
-    "UserName":'User4',
-    "AHT":'0000000',
-    "FCR":'000%',
-    "Quality":'000%',
-    'Service_level':'000%',
-    'Performance':'Moderate'
+    "UserName": 'User4',
+    "AHT": '0000000',
+    "FCR": '000%',
+    "Quality": '000%',
+    'Service_level': '000%',
+    'Performance': 'Moderate',
+    'process': [{
+      "UserName": 'User1',
+      "AHT": '0000000',
+      "FCR": '000%',
+      "Quality": '000%',
+      'Service_level': '000%',
+      'Performance': 'High',
+      'subprocess': [{
+        "UserName": 'User1',
+        "AHT": '0000000',
+        "FCR": '000%',
+        "Quality": '000%',
+        'Service_level': '000%',
+        'Performance': 'High',
+        'stagewise': [{
+          "UserName": 'User1',
+          "AHT": '0000000',
+          "FCR": '000%',
+          "Quality": '000%',
+          'Service_level': '000%',
+          'Performance': 'High',
+          'Batchwise': [{
+            "UserName": 'User1',
+            "AHT": '0000000',
+            "FCR": '000%',
+            "Quality": '000%',
+            'Service_level': '000%',
+            'Performance': 'High',
+            'Userwise': [{
+              "UserName": 'User1',
+              "AHT": '0000000',
+              "FCR": '000%',
+              "Quality": '000%',
+              'Service_level': '000%',
+              'Performance': 'High',
+
+            }]
+
+          }]
+
+        }]
+
+
+      }]
+
+
+    }]
   },
   {
-    "UserName":'User5',
-    "AHT":'0000000',
-    "FCR":'000%',
-    "Quality":'000%',
-    'Service_level':'000%',
-    'Performance':'Low'
+    "UserName": 'User5',
+    "AHT": '0000000',
+    "FCR": '000%',
+    "Quality": '000%',
+    'Service_level': '000%',
+    'Performance': 'Low',
+    'process': [{
+      "UserName": 'User1',
+      "AHT": '0000000',
+      "FCR": '000%',
+      "Quality": '000%',
+      'Service_level': '000%',
+      'Performance': 'High',
+      'subprocess': [{
+        "UserName": 'User1',
+        "AHT": '0000000',
+        "FCR": '000%',
+        "Quality": '000%',
+        'Service_level': '000%',
+        'Performance': 'High',
+        'stagewise': [{
+          "UserName": 'User1',
+          "AHT": '0000000',
+          "FCR": '000%',
+          "Quality": '000%',
+          'Service_level': '000%',
+          'Performance': 'High',
+          'Batchwise': [{
+            "UserName": 'User1',
+            "AHT": '0000000',
+            "FCR": '000%',
+            "Quality": '000%',
+            'Service_level': '000%',
+            'Performance': 'High',
+            'Userwise': [{
+              "UserName": 'User1',
+              "AHT": '0000000',
+              "FCR": '000%',
+              "Quality": '000%',
+              'Service_level': '000%',
+              'Performance': 'High',
+
+            }]
+
+          }]
+
+        }]
+
+
+      }]
+
+
+    }]
   },
   {
-    "UserName":'User6',
-    "AHT":'0000000',
-    "FCR":'000%',
-    "Quality":'000%',
-    'Service_level':'000%',
-    'Performance':'Low'
+    "UserName": 'User6',
+    "AHT": '0000000',
+    "FCR": '000%',
+    "Quality": '000%',
+    'Service_level': '000%',
+    'Performance': 'Low',
+    'process': [{
+      "UserName": 'User1',
+      "AHT": '0000000',
+      "FCR": '000%',
+      "Quality": '000%',
+      'Service_level': '000%',
+      'Performance': 'High',
+      'subprocess': [{
+        "UserName": 'User1',
+        "AHT": '0000000',
+        "FCR": '000%',
+        "Quality": '000%',
+        'Service_level': '000%',
+        'Performance': 'High',
+        'stagewise': [{
+          "UserName": 'User1',
+          "AHT": '0000000',
+          "FCR": '000%',
+          "Quality": '000%',
+          'Service_level': '000%',
+          'Performance': 'High',
+          'Batchwise': [{
+            "UserName": 'User1',
+            "AHT": '0000000',
+            "FCR": '000%',
+            "Quality": '000%',
+            'Service_level': '000%',
+            'Performance': 'High',
+            'Userwise': [{
+              "UserName": 'User1',
+              "AHT": '0000000',
+              "FCR": '000%',
+              "Quality": '000%',
+              'Service_level': '000%',
+              'Performance': 'High',
+
+            }]
+
+          }]
+
+        }]
+
+
+      }]
+
+
+    }]
   }
-]
+  ]
 
 
 tableContentForEngagement=[{
@@ -141,6 +579,7 @@ tableContentForEngagement=[{
 
   ngOnInit():void{
     this.selectedValue = 'All Vendorwise Report';
+    this.currentLevel = this.tableContent
     
     
   }
@@ -150,6 +589,9 @@ tableContentForEngagement=[{
     switch (value) {
         case 'All Vendorwise Report':
             this.tableTitleValue = 'Vendors';
+            this.currentLevel=this.tableContent;
+
+            this.level=0;
             break;
         case 'All Processwise Report':
             this.tableTitleValue = 'Process';
@@ -168,6 +610,7 @@ tableContentForEngagement=[{
             break;
         default:
             this.tableTitleValue = 'Vendors';
+            this.level=0;
             break;
     }
 
@@ -200,10 +643,70 @@ tableContentForEngagement=[{
     this.authService.logout()
     this._router.navigateByUrl('')
   }
-  openInfoFormTable(index:any){
-    let newIndex=index+1;
-    this.selectedFromTable=this.tableTitleValue + newIndex;
+  openInfoFormTable(index: any) {
+    let newIndex = index + 1;
+    this.selectedFromTable = this.tableTitleValue + newIndex;
+    
+    // Make sure this.tableContent is not null and index is valid
+    if (this.tableContent && index >= 0 && index < this.tableContent.length) {
+      const selectedData = this.tableContent[index];
+    
+      
+      // Assuming selectedData contains nested objects
+      if (selectedData && selectedData.process) {
+        
+        if(this.level=='1'){
+          this.tableTitleValue = "Process";
+        this.process = selectedData.process;
+        this.currentLevel = this.process;
+        this.level++;
+        }
+        else if(this.level=='2'){
+          this.subprocess = this.process[index]?.subprocess;
+          this.tableTitleValue = "sub-Process";
+          this.currentLevel = this.subprocess;
+          console.log(this.currentLevel)
+          this.level++;
+          console.log(this.level)
 
+        }
+        else if(this.level=='3'){
+          this.stagewise = this.subprocess[index]?.stagewise;
+          this.tableTitleValue = "Stagewise";
+          this.currentLevel = this.stagewise;
+          
+          this.level++;
+          console.log(this.level)
+          
+        }
+        else if(this.level=='4'){
+          this.Batchwise= this.stagewise[index]?.Batchwise;
+          this.tableTitleValue = "Batchwise";
+          this.currentLevel = this.Batchwise;
+          
+          this.level++;
+
+        }
+        else if(this.level=='5'){
+          this.userwise= this.Batchwise[index]?.Userwise;
+          this.tableTitleValue = "Userwise";
+          this.currentLevel = this.userwise;
+          
+          // this.level=0;
+
+        }
+        else {
+          this.level=0
+        }
+        
+        // console.log(this.process[0]?.find('subprocess'))
+  
+      
+      }
+    }
   }
+  
+  
+  
 
 }
