55075e645475cb22e86659269bff3fc9be3ee426
Rajat Bhat <rajat@thegamificationcompany.com>
2023-09-12 10:34:35 +0530

    changes

--- src/app/component/side-navigation/setup/setup.component.ts
+++ src/app/component/side-navigation/setup/setup.component.ts
@@ -397,7 +397,8 @@ export class SetupComponent implements OnInit {
     });
 
     // You can pass data to the modal if needed
-    modalRef.componentInstance.someData = this.createOrgResponse;
+    modalRef.componentInstance.someData =
+      'Done! The Organization has been created successfully.';
     modalRef.componentInstance.screen = 'Organization';
   }
 
@@ -459,8 +460,6 @@ export class SetupComponent implements OnInit {
     this.contactEmailEdit = this.getEditDetails?.contactEmail;
     this.multiFieldForm.get('contactEmail')?.setValue(this.contactEmailEdit);
     this.multiFieldForm.get('contactEmail')?.value || '';
-
-    
   }
 
   generateRandom8DigitNumber(): void {
